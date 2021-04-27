afc549ac6b0853baaaf7b88a421f42fb3ec1d6b0
David Daghelian <dhd@rogers.com>
2021-04-27 01:52:32 -0400

    Changed the bot channels dict to keep guild id The dictionary / json file that keeps track of the bot channel for each server now uses guild id instead of the guild class (since the guild class does not work with json dumps).

--- movie_lottery_bot.py
+++ movie_lottery_bot.py
@@ -135,12 +135,12 @@ class MovieLotteryClient(discord.Client):
             await message.channel.send("Sorry, that doesn't look like a channel name")
             return
         # Save the channel to the channels dictionary
-        channels[message.guild] = channel_name[1].strip().split(" ")
+        self.channels[message.guild.id] = channel_name[1].strip().split(" ")[0]
         # Save the channels dictionary to a file
         await self.save_info(self.channels, "channels.json")
         # Notify the user & the server that their bot channel was changed
         await message.channel.send("I have changed your bot channel for your movie lottery list")
-        print('Changing ', message.guild, "'s bot channel to", channels[message.guild])         
+        print('Changing ' + str(message.guild) + "'s bot channel to", self.channels[message.guild.id])         
     
     # The list of functions that are callable through commands     
     funcs_check = {"add": add_to_lst, "list": get_lst, "reset": reset_lst, "pick": pick_movie, "remove": remove_movie}
@@ -181,7 +181,7 @@ class MovieLotteryClient(discord.Client):
             return
 
         # Ignore messages that aren't in the bot's channel
-        if str(message.channel) == self.channels.get(message.guild, "bots"):
+        if str(message.channel) == self.channels.get(message.guild.id, "bots"):
             func = await self.__check_command(message, self.funcs_check)            
             if func != None:
                 await func(self, message)
